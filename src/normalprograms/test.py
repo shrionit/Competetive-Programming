4237c44a37522fc57c5473c5ae3fff82dc396b28
Shriprakash Tiwari <sptiwari46289@gmail.com>
2019-09-25 00:05:52 +0530

    recreated with columns

--- src/normalprograms/test.py
+++ src/normalprograms/test.py
@@ -9,45 +9,24 @@ from sklearn.decomposition import PCA
 
 
 
-bdata = [None] * 21
-with open('../Data_Clustering/Autism-Screening-Child-Data Plus Description/Autism-Child-Data.arff', 'r') as file:
+bdata = {}
+with open('../Autism-Screening-Child-Data Plus Description/Autism-Child-Data.arff', 'r') as file:
 	for line in file:
 		line = line.strip('\n')
 		if not line:
 			continue
-		f = [i for i in list(line.split(' ')[0])]
-		if f[0] == '@':
-			continue
-		linedata = line.split(',')
-		for i in range(len(linedata)):
-			if bdata[i] == None:
-				bdata[i] = []
-				bdata[i].append(str(linedata[i]))
+		if line.startswith('@attribute'):
+			line = line.split(' ')[1]
+			bdata[line] = []
+		if not line.startswith('@'):
+			linedata = line.split(',')
+			if not (len(linedata) == 21):
+				continue
 			else:
-				bdata[i].append(str(linedata[i]))
+				i=0
+				for c in bdata:
+					bdata[c].append(linedata[i])
+					i = i+1
 
-
-del bdata[11:21]
-for i in range(len(bdata)):
-	for j in range(len(bdata[i])):
-		if not re.search('[0-9]', bdata[i][j]):
-			bdata[i][j] = 0
-		else:
-			bdata[i][j] = int(bdata[i][j])
-bdata = np.array(bdata)
-bdata = tuple(map(tuple, bdata))
-
-
-
-clf = KMeans(n_clusters = 2)
-clf.fit(bdata)
-
-centroids = clf.cluster_centers_
-label = clf.labels_
-
-colors = ["r.", "c.", "g.", "k.", "p."]
-
-for i in range(len(bdata)-1):
-	plt.plot(bdata[i], bdata[i+1], colors[label[i]], markersize=10)
-plt.scatter(centroids[:,0], centroids[:,1], marker="x", s=150, linewidths = 5)
+plt.plot(bdata['A1_Score'], bdata['A2_Score'], c='r')
 plt.show()
